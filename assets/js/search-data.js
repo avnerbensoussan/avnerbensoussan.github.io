7f80b21388536db6cf7d7cc49141b0926150b9ed
avnerbensoussan <91475096+avnerbensoussan@users.noreply.github.com>
2026-01-17 13:23:39 +0000

    Deploying to gh-pages from @ avnerbensoussan/avnerbensoussan.github.io@8b0e89babe7c2d89499417427c324b5fd9697d17 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,8 +16,15 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-teaching",
-          title: "teaching",
+        },{id: "nav-repositories",
+          title: "repositories",
+          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/repositories/";
+          },
+        },{id: "nav-presentations",
+          title: "presentations",
           description: "Latest Teaching Experience",
           section: "Navigation",
           handler: () => {

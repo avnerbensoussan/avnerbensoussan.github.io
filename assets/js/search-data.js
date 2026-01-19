4bc53b353ed6bd1b220424fa0be0f04fe5a6dc1a
avnerbensoussan <91475096+avnerbensoussan@users.noreply.github.com>
2026-01-19 08:10:14 +0000

    Deploying to gh-pages from @ avnerbensoussan/avnerbensoussan.github.io@f90a93a5ed412ee42c81de522aa31656f984b9c9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,13 +37,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/presentations/";
           },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/people/";
-          },
         },{
         id: 'social-cv',
         title: 'CV',
